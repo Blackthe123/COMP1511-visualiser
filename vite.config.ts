3289d2234c87350b5550705f3b576bb9fbc260fb
Ayush Yajaman <ayush.yajaman@gmail.com>
2026-07-31 12:54:36 +1000

    Removed unnecessary comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// NOTE: 'base' must match your GitHub repository name exactly
 export default defineConfig({
   plugins: [react()],
   base: '/COMP1511-visualiser/',
